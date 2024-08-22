5d277de25d47951a540f45ebef75edb4705c4143
dacronik <dacronik@gmail.com>
2024-08-22 15:05:24 -0400

    segunda parte del trabajo,agregando manejo de formularios

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,223 +1,223 @@
-$(document).ready(function() {
-    let table = $('#resultadosTable').DataTable();
-    let empresas = [];
-    let contadorEmpresaId = 1;
-    let contadorImportacionId = 1;
-    let empresaEditando = null;
-    let importacionEditando = null;
+// $(document).ready(function() {
+//     let table = $('#resultadosTable').DataTable();
+//     let empresas = [];
+//     let contadorEmpresaId = 1;
+//     let contadorImportacionId = 1;
+//     let empresaEditando = null;
+//     let importacionEditando = null;
 
-    class Empresa {
-        constructor(id, nombre, rut) {
-            this.id = id;
-            this.nombre = nombre;
-            this.rut = rut;
-            this.importaciones = [];
-        }
+//     class Empresa {
+//         constructor(id, nombre, rut) {
+//             this.id = id;
+//             this.nombre = nombre;
+//             this.rut = rut;
+//             this.importaciones = [];
+//         }
 
-        agregarImportacion(importacion) {
-            this.importaciones.push(importacion);
-        }
+//         agregarImportacion(importacion) {
+//             this.importaciones.push(importacion);
+//         }
 
-        sumaTotalImportaciones() {
-            return this.importaciones.reduce((total, imp) => total + (imp.precioUnitario * imp.numeroProductos), 0);
-        }
+//         sumaTotalImportaciones() {
+//             return this.importaciones.reduce((total, imp) => total + (imp.precioUnitario * imp.numeroProductos), 0);
+//         }
 
-        sumaTotalProductos() {
-            return this.importaciones.reduce((total, imp) => total + imp.numeroProductos, 0);
-        }
+//         sumaTotalProductos() {
+//             return this.importaciones.reduce((total, imp) => total + imp.numeroProductos, 0);
+//         }
 
-        getNombre() {
-            return this.nombre;
-        }
-        setNombre(nombre) {
-            this.nombre = nombre;
-        }
-        getRut() {
-            return this.rut;
-        }
-        setRut(rut) {
-            this.rut = rut;
-        }
-    }
+//         getNombre() {
+//             return this.nombre;
+//         }
+//         setNombre(nombre) {
+//             this.nombre = nombre;
+//         }
+//         getRut() {
+//             return this.rut;
+//         }
+//         setRut(rut) {
+//             this.rut = rut;
+//         }
+//     }
 
-    class Importacion {
-        constructor(id, producto, numeroProductos, precioUnitario) {
-            this.id = id;
-            this.producto = producto;
-            this.numeroProductos = numeroProductos;
-            this.precioUnitario = precioUnitario;
-        }
+//     class Importacion {
+//         constructor(id, producto, numeroProductos, precioUnitario) {
+//             this.id = id;
+//             this.producto = producto;
+//             this.numeroProductos = numeroProductos;
+//             this.precioUnitario = precioUnitario;
+//         }
 
-        getProducto() {
-            return this.producto;
-        }
-        setProducto(producto) {
-            this.producto = producto;
-        }
-        getNumeroProductos() {
-            return this.numeroProductos;
-        }
-        setNumeroProductos(numeroProductos) {
-            this.numeroProductos = numeroProductos;
-        }
-        getPrecioUnitario() {
-            return this.precioUnitario;
-        }
-        setPrecioUnitario(precioUnitario) {
-            this.precioUnitario = precioUnitario;
-        }
-    }
+//         getProducto() {
+//             return this.producto;
+//         }
+//         setProducto(producto) {
+//             this.producto = producto;
+//         }
+//         getNumeroProductos() {
+//             return this.numeroProductos;
+//         }
+//         setNumeroProductos(numeroProductos) {
+//             this.numeroProductos = numeroProductos;
+//         }
+//         getPrecioUnitario() {
+//             return this.precioUnitario;
+//         }
+//         setPrecioUnitario(precioUnitario) {
+//             this.precioUnitario = precioUnitario;
+//         }
+//     }
 
-    $('#itemTipo').change(function() {
-        const tipo = $(this).val();
-        if (tipo === 'empresa') {
-            $('#empresaFields').removeClass('d-none');
-            $('#importacionFields').addClass('d-none');
-            $('#empresaSelect').addClass('d-none');
-        } else if (tipo === 'importacion') {
-            $('#empresaFields').addClass('d-none');
-            $('#importacionFields').removeClass('d-none');
-            $('#empresaSelect').removeClass('d-none');
-            $('#empresaSelect').empty();  // Limpiar el select de empresas
-            empresas.forEach(emp => {
-                $('#empresaSelect').append(`<option value="${emp.id}">${emp.nombre}</option>`);
-            });
-        }
-    });
+//     $('#itemTipo').change(function() {
+//         const tipo = $(this).val();
+//         if (tipo === 'empresa') {
+//             $('#empresaFields').removeClass('d-none');
+//             $('#importacionFields').addClass('d-none');
+//             $('#empresaSelect').addClass('d-none');
+//         } else if (tipo === 'importacion') {
+//             $('#empresaFields').addClass('d-none');
+//             $('#importacionFields').removeClass('d-none');
+//             $('#empresaSelect').removeClass('d-none');
+//             $('#empresaSelect').empty();  // Limpiar el select de empresas
+//             empresas.forEach(emp => {
+//                 $('#empresaSelect').append(`<option value="${emp.id}">${emp.nombre}</option>`);
+//             });
+//         }
+//     });
 
-    $('#formulario').on('submit', function(e) {
-        e.preventDefault();
+//     $('#formulario').on('submit', function(e) {
+//         e.preventDefault();
 
-        const tipo = $('#itemTipo').val();
-        if (tipo === 'empresa') {
-            const nombre = $('#empresaNombre').val();
-            const rut = $('#empresaRut').val();
-            if (nombre && rut) {
-                if (empresaEditando) {
-                    empresaEditando.setNombre(nombre);
-                    empresaEditando.setRut(rut);
-                    empresaEditando = null;
-                } else {
-                    const nuevaEmpresa = new Empresa(contadorEmpresaId++, nombre, rut);
-                    empresas.push(nuevaEmpresa);
-                }
-                $('#empresaNombre').val('');
-                $('#empresaRut').val('');
-            } else {
-                alert('Por favor, complete todos los campos de la empresa.');
-                return;
-            }
-        } else if (tipo === 'importacion') {
-            const producto = $('#importacionProducto').val();
-            const cantidad = parseInt($('#importacionCantidad').val(), 10);
-            const precio = parseFloat($('#importacionPrecio').val());
-            const empresaId = $('#empresaSelect').val();
-            const id = $('#itemId').val();
+//         const tipo = $('#itemTipo').val();
+//         if (tipo === 'empresa') {
+//             const nombre = $('#empresaNombre').val();
+//             const rut = $('#empresaRut').val();
+//             if (nombre && rut) {
+//                 if (empresaEditando) {
+//                     empresaEditando.setNombre(nombre);
+//                     empresaEditando.setRut(rut);
+//                     empresaEditando = null;
+//                 } else {
+//                     const nuevaEmpresa = new Empresa(contadorEmpresaId++, nombre, rut);
+//                     empresas.push(nuevaEmpresa);
+//                 }
+//                 $('#empresaNombre').val('');
+//                 $('#empresaRut').val('');
+//             } else {
+//                 alert('Por favor, complete todos los campos de la empresa.');
+//                 return;
+//             }
+//         } else if (tipo === 'importacion') {
+//             const producto = $('#importacionProducto').val();
+//             const cantidad = parseInt($('#importacionCantidad').val(), 10);
+//             const precio = parseFloat($('#importacionPrecio').val());
+//             const empresaId = $('#empresaSelect').val();
+//             const id = $('#itemId').val();
 
-            if (!producto || isNaN(cantidad) || isNaN(precio)) {
-                alert('Por favor, complete todos los campos de la importación.');
-                return;
-            }
+//             if (!producto || isNaN(cantidad) || isNaN(precio)) {
+//                 alert('Por favor, complete todos los campos de la importación.');
+//                 return;
+//             }
 
-            const empresa = empresas.find(emp => emp.id == empresaId);
-            if (empresa) {
-                if (id) {
-                    const importacion = empresa.importaciones.find(imp => imp.id == id);
-                    if (importacion) {
-                        importacion.setProducto(producto);
-                        importacion.setNumeroProductos(cantidad);
-                        importacion.setPrecioUnitario(precio);
-                    }
-                } else {
-                    const nuevaImportacion = new Importacion(contadorImportacionId++, producto, cantidad, precio);
-                    empresa.agregarImportacion(nuevaImportacion);
-                }
-                $('#importacionProducto').val('');
-                $('#importacionCantidad').val('');
-                $('#importacionPrecio').val('');
-                $('#empresaSelect').val('');
-                $('#itemId').val('');
-            } else {
-                alert('Por favor, seleccione una empresa para la importación.');
-                return;
-            }
-        }
-        actualizarTabla();
-    });
+//             const empresa = empresas.find(emp => emp.id == empresaId);
+//             if (empresa) {
+//                 if (id) {
+//                     const importacion = empresa.importaciones.find(imp => imp.id == id);
+//                     if (importacion) {
+//                         importacion.setProducto(producto);
+//                         importacion.setNumeroProductos(cantidad);
+//                         importacion.setPrecioUnitario(precio);
+//                     }
+//                 } else {
+//                     const nuevaImportacion = new Importacion(contadorImportacionId++, producto, cantidad, precio);
+//                     empresa.agregarImportacion(nuevaImportacion);
+//                 }
+//                 $('#importacionProducto').val('');
+//                 $('#importacionCantidad').val('');
+//                 $('#importacionPrecio').val('');
+//                 $('#empresaSelect').val('');
+//                 $('#itemId').val('');
+//             } else {
+//                 alert('Por favor, seleccione una empresa para la importación.');
+//                 return;
+//             }
+//         }
+//         actualizarTabla();
+//     });
 
-    function actualizarTabla() {
-        table.clear().draw();
+//     function actualizarTabla() {
+//         table.clear().draw();
 
-        empresas.forEach(empresa => {
-            table.row.add([
-                empresa.id,
-                empresa.getNombre(),
-                empresa.getRut(),
-                empresa.sumaTotalImportaciones(),
-                empresa.sumaTotalProductos(),
-                `<a href="#" class="edit-empresa" data-id="${empresa.id}"><i class="fas fa-edit"></i></a>
-                 <a href="#" class="delete-empresa" data-id="${empresa.id}"><i class="fas fa-trash"></i></a>`
-            ]).draw();
+//         empresas.forEach(empresa => {
+//             table.row.add([
+//                 empresa.id,
+//                 empresa.getNombre(),
+//                 empresa.getRut(),
+//                 empresa.sumaTotalImportaciones(),
+//                 empresa.sumaTotalProductos(),
+//                 `<a href="#" class="edit-empresa" data-id="${empresa.id}"><i class="fas fa-edit"></i></a>
+//                  <a href="#" class="delete-empresa" data-id="${empresa.id}"><i class="fas fa-trash"></i></a>`
+//             ]).draw();
 
-            empresa.importaciones.forEach(importacion => {
-                table.row.add([
-                    importacion.id,
-                    importacion.producto,
-                    '',
-                    '',
-                    importacion.numeroProductos,
-                    `<a href="#" class="edit-importacion" data-id="${importacion.id}"><i class="fas fa-edit"></i></a>
-                     <a href="#" class="delete-importacion" data-id="${importacion.id}"><i class="fas fa-trash"></i></a>`
-                ]).draw();
-            });
-        });
-    }
+//             empresa.importaciones.forEach(importacion => {
+//                 table.row.add([
+//                     importacion.id,
+//                     importacion.producto,
+//                     '',
+//                     '',
+//                     importacion.numeroProductos,
+//                     `<a href="#" class="edit-importacion" data-id="${importacion.id}"><i class="fas fa-edit"></i></a>
+//                      <a href="#" class="delete-importacion" data-id="${importacion.id}"><i class="fas fa-trash"></i></a>`
+//                 ]).draw();
+//             });
+//         });
+//     }
 
-    $('#resultadosTable').on('click', '.edit-empresa', function(e) {
-        e.preventDefault();
-        const id = $(this).data('id');
-        empresaEditando = empresas.find(emp => emp.id == id);
-        if (empresaEditando) {
-            $('#itemTipo').val('empresa').change();
-            $('#empresaNombre').val(empresaEditando.getNombre());
-            $('#empresaRut').val(empresaEditando.getRut());
-            $('#itemId').val(empresaEditando.id);
-        }
-    });
+//     $('#resultadosTable').on('click', '.edit-empresa', function(e) {
+//         e.preventDefault();
+//         const id = $(this).data('id');
+//         empresaEditando = empresas.find(emp => emp.id == id);
+//         if (empresaEditando) {
+//             $('#itemTipo').val('empresa').change();
+//             $('#empresaNombre').val(empresaEditando.getNombre());
+//             $('#empresaRut').val(empresaEditando.getRut());
+//             $('#itemId').val(empresaEditando.id);
+//         }
+//     });
 
-    $('#resultadosTable').on('click', '.delete-empresa', function(e) {
-        e.preventDefault();
-        const id = $(this).data('id');
-        if (confirm('¿Estás seguro de que deseas eliminar esta empresa?')) {
-            empresas = empresas.filter(emp => emp.id !== id);
-            actualizarTabla();
-        }
-    });
+//     $('#resultadosTable').on('click', '.delete-empresa', function(e) {
+//         e.preventDefault();
+//         const id = $(this).data('id');
+//         if (confirm('¿Estás seguro de que deseas eliminar esta empresa?')) {
+//             empresas = empresas.filter(emp => emp.id !== id);
+//             actualizarTabla();
+//         }
+//     });
 
-    $('#resultadosTable').on('click', '.edit-importacion', function(e) {
-        e.preventDefault();
-        const id = $(this).data('id');
-        const empresa = empresas.find(emp => emp.importaciones.some(imp => imp.id == id));
-        if (empresa) {
-            importacionEditando = empresa.importaciones.find(imp => imp.id == id);
-            if (importacionEditando) {
-                $('#itemTipo').val('importacion').change();
-                $('#importacionProducto').val(importacionEditando.getProducto());
-                $('#importacionCantidad').val(importacionEditando.getNumeroProductos());
-                $('#importacionPrecio').val(importacionEditando.getPrecioUnitario());
-                $('#empresaSelect').val(empresa.id);
-                $('#itemId').val(importacionEditando.id);
-            }
-        }
-    });
+//     $('#resultadosTable').on('click', '.edit-importacion', function(e) {
+//         e.preventDefault();
+//         const id = $(this).data('id');
+//         const empresa = empresas.find(emp => emp.importaciones.some(imp => imp.id == id));
+//         if (empresa) {
+//             importacionEditando = empresa.importaciones.find(imp => imp.id == id);
+//             if (importacionEditando) {
+//                 $('#itemTipo').val('importacion').change();
+//                 $('#importacionProducto').val(importacionEditando.getProducto());
+//                 $('#importacionCantidad').val(importacionEditando.getNumeroProductos());
+//                 $('#importacionPrecio').val(importacionEditando.getPrecioUnitario());
+//                 $('#empresaSelect').val(empresa.id);
+//                 $('#itemId').val(importacionEditando.id);
+//             }
+//         }
+//     });
 
-    $('#resultadosTable').on('click', '.delete-importacion', function(e) {
-        e.preventDefault();
-        const id = $(this).data('id');
-        const empresa = empresas.find(emp => emp.importaciones.some(imp => imp.id == id));
-        if (empresa) {
-            empresa.importaciones = empresa.importaciones.filter(imp => imp.id !== id);
-            actualizarTabla();
-        }
-    });
-});
+//     $('#resultadosTable').on('click', '.delete-importacion', function(e) {
+//         e.preventDefault();
+//         const id = $(this).data('id');
+//         const empresa = empresas.find(emp => emp.importaciones.some(imp => imp.id == id));
+//         if (empresa) {
+//             empresa.importaciones = empresa.importaciones.filter(imp => imp.id !== id);
+//             actualizarTabla();
+//         }
+//     });
+// });
--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,205 @@ console.log('Total de importaciones: ', empresa1.TotalImports);
 console.log('Total de Productos: ', empresa1.TotalProducts);
 console.log('Total Precio: ', empresa1.totalPrice);
 console.log('Importaciones',empresa1.importacionesInfo);
+
+//Parte 2 de trabajo manejo de formularios
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const itemTipo = document.getElementById('itemTipo');
+    const empresaFields = document.getElementById('empresaFields');
+    const importacionFields = document.getElementById('importacionFields');
+    const formulario = document.getElementById('formulario');
+    const empresaSelect = document.getElementById('empresaSelect');
+    const empresaTableBody = document.getElementById('empresaTableBody');
+
+    let empresas = [];
+
+
+    //Detectando los cambios de tipo en select
+    itemTipo.addEventListener('change', () =>{
+        const selectdType = itemTipo.value;
+
+        //Mostrar u ocultar campos basados en la selección
+        if(selectdType === 'empresa'){
+            empresaFields.classList.remove('d-none');
+            importacionFields.classList.add('d-none');
+        } else if(selectdType === 'importacion'){
+            empresaFields.classList.add('d-none');
+            importacionFields.classList.remove('d-none');
+        }else{
+            empresaFields.classList.add('d-none');
+            importacionFields.classList.add('d-none');
+        }
+    });
+
+    //Manejo del formulario
+    formulario.addEventListener('submit', (event) =>{
+        event.preventDefault();
+
+        const selectdType = itemTipo.value;
+
+        if(selectdType === 'empresa'){
+            //Obtener los valores del campo empresa
+            const nombre = document.getElementById('empresaNombre').value;
+            const rut = document.getElementById('empresaRut').value;
+
+            //Crear una nueva instancia de empresa
+            const nuevaEmpresa = new Empresa(generateId(), nombre, rut)
+            empresas.push(nuevaEmpresa);
+
+            console.log("Empresa agregada:", nuevaEmpresa);
+            console.log('lista de empresas:', empresas);
+
+            //Actualizar el select de empresas
+            actualizarEmpresaSelect();
+
+            //Actualizar la tabla
+            actualizarTablaEmpresas()
+
+
+            //Limpiar el formulario
+            formulario.reset();
+            empresaFields.classList.add('d-none');
+            Swal.fire({
+                title: `Empresa ${nuevaEmpresa.name} agregada con exito`,
+                icon: 'success',
+                timer: 2000,
+                timerProgressBar: true
+            })
+        }else if (selectdType === 'importacion'){
+            //Obtenemos los valores de los campos de importación
+            const producto = document.getElementById('importacionProducto').value;
+            const cantidad = parseInt(document.getElementById('importacionCantidad').value);
+            const precio = parseFloat(document.getElementById('importacionPrecio').value);
+            const empresaId = empresaSelect.value;
+
+            //Buscamos la empresa seleccionada
+            const empresaSeleccionada = empresas.find(empresa => empresa.id === empresaId);
+
+            if(empresaSeleccionada) {
+                //Creamos una nueva instancia de importación
+                const nuevaImportacion = new Importacion(generateId(),producto,cantidad,precio);
+
+                //añadimos la importación a la empresa
+                empresaSeleccionada.addImport(nuevaImportacion);
+
+                console.log("Importación agregada:",nuevaImportacion);
+                console.log('Importacioones de la empresa:', empresaSeleccionada.imports);
+
+                //Actualizar la tabla
+                actualizarTablaEmpresas()
+
+                //Limpiamos los campos del formulario y damos info al usuario de que agrego una importación
+                formulario.reset();
+                importacionFields.classList.add('d-none');
+                Swal.fire({
+                    title: `Importación ${nuevaImportacion.product} agregada con exito`,
+                    icon: 'success',
+                    timer: 2000,
+                    timerProgressBar: true
+                })
+            } else{
+                Swal.fire({
+                    title:'Error: Empresa no seleccionada o no encontrada.',
+                    icon: 'error',
+                })
+            }
+        }
+    });
+
+
+    //Función para actualizar el select de empresas
+    function actualizarEmpresaSelect(){
+        empresaSelect.innerHTML = `<option value="" disabled selected>Seleccione una empresa</option>`;
+        empresas.forEach(empresa =>{
+            const option = document.createElement('option');
+            option.value = empresa.id;
+            option.textContent = `${empresa.name} (${empresa.rut})`;
+            empresaSelect.appendChild(option);
+        });
+
+        //console.log("Select de empresas actualizado:", empresaSelect.innerHTML);
+    };
+
+    //Función para actualizar la tabla de empresas.
+    function actualizarTablaEmpresas(){
+        empresaTableBody.innerHTML = ''; //Limpiamos la tabla
+
+        empresas.forEach(empresa =>{
+            const fila = document.createElement('tr');
+
+            fila.innerHTML = `
+                <td>${empresa.id}</td>
+                <td>${empresa.name}</td>
+                <td>${empresa.rut}</td>
+                <td>${empresa.TotalImports}</td>
+                <td>${empresa.TotalProducts}</td>
+                <td>
+                    <button class="btn btn-warning btn-sm editarEmpresa" data-id="${empresa.id}">
+                        <i class="fas fa-pencil-alt"></i>
+                    </button>
+                    <button class="btn btn-danger btn-sm eliminarEmpresa" data-id="${empresa.id}">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </td>
+            `;
+            empresaTableBody.appendChild(fila);
+        });
+
+        // Agregar eventos a los botones de editar y eliminar
+        document.querySelectorAll('.editarEmpresa').forEach(btn => {
+            btn.addEventListener('click', editarEmpresa);
+        });
+
+        document.querySelectorAll('.eliminarEmpresa').forEach(btn => {
+            btn.addEventListener('click', eliminarEmpresa);
+        });
+
+        console.log("Tabla de empresas actualizada.");
+    };
+
+
+    // función para editar 
+    function editarEmpresa(event) {
+
+        const empresaId = event.target.getAttribute('data-id');
+        const empresa = empresas.find(empresa => empresa.id === empresaId);
+
+        Swal.fire({
+            title: 'Editar empresa',
+            text: `Aquí podrías implementar un formulario de edición para la empresa ${empresa.name}.`,
+            icon: 'info',
+            showConfirmButton: true,
+        })
+    };
+
+    // función para eliminar una empresa
+    function eliminarEmpresa(event) {
+        const empresaId = event.target.getAttribute('data-id');
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "No podrás revertir esta acción",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                empresas = empresas.filter(empresa => empresa.id !== empresaId);
+                actualizarTablaEmpresas();
+
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Eliminado',
+                    text: 'La empresa ha sido eliminada.',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
+                console.log("Empresa eliminada:", empresaId);
+            }
+        });
+    }
+})
